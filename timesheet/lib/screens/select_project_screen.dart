922d340613416e10b4414495fdde04b04e4d2fba
Ammara Akbar <107490940+Ammara-Akbar@users.noreply.github.com>
2025-09-29 06:56:19 -0700

    memo dialog box

--- timesheet/lib/screens/select_project_screen.dart
+++ timesheet/lib/screens/select_project_screen.dart
@@ -1,29 +1,69 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:timesheet/screens/home_screen.dart';
 import 'package:timesheet/utils/colors.dart';
 
-class SelectProjectScreen extends StatelessWidget {
+class SelectProjectScreen extends StatefulWidget {
   const SelectProjectScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<String> projects = [
-      "Project A",
-      "Project B",
-      "Project C",
-      "Project D",
-      "Project E",
-      "Project F",
-    ];
+  State<SelectProjectScreen> createState() => _SelectProjectScreenState();
+}
 
+class _SelectProjectScreenState extends State<SelectProjectScreen> {
+  final List<String> projects = [
+    "Project A",
+    "Project B",
+    "Project C",
+    "Project D",
+    "Project E",
+    "Project F",
+  ];
+
+  int? runningIndex;
+  Duration elapsed = Duration.zero;
+  Timer? _timer;
+
+  String _formatDuration(Duration d) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    return "${twoDigits(d.inHours)}:${twoDigits(d.inMinutes.remainder(60))}:${twoDigits(d.inSeconds.remainder(60))}";
+  }
+
+  void _startTimer(int index) {
+    setState(() {
+      runningIndex = index;
+      elapsed = Duration.zero;
+    });
+    _timer?.cancel();
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setState(() {
+        elapsed += const Duration(seconds: 1);
+      });
+    });
+  }
+
+  void _pauseTimer() {
+    _timer?.cancel();
+    setState(() {
+      runningIndex = null;
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        // 👇 Here you control what happens when back is pressed
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (_) => const HomeScreen()),
         );
-        return false; // Prevents default back action (we already handled it)
+        return false;
       },
       child: Scaffold(
         backgroundColor: Colors.white,
@@ -59,7 +99,7 @@ class SelectProjectScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // 🔍 Search Bar
+              // Search Bar
               TextField(
                 cursorColor: AppColors.primaryColor,
                 decoration: InputDecoration(
@@ -84,10 +124,9 @@ class SelectProjectScreen extends StatelessWidget {
                   ),
                 ),
               ),
-
               const SizedBox(height: 20),
 
-              // 📌 Section Title
+              // Section Title
               const Text(
                 "Active Projects",
                 style: TextStyle(
@@ -98,12 +137,13 @@ class SelectProjectScreen extends StatelessWidget {
               ),
               const SizedBox(height: 12),
 
-              // 📋 Project List
+              // Project List
               Expanded(
                 child: ListView.separated(
                   itemCount: projects.length,
                   separatorBuilder: (_, __) => const SizedBox(height: 10),
                   itemBuilder: (context, index) {
+                    final isRunning = runningIndex == index;
                     return Container(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 14, vertical: 16),
@@ -112,32 +152,58 @@ class SelectProjectScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(8),
                       ),
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           // Project Title
-                          Text(
-                            projects[index],
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black,
+                          Expanded(
+                            child: Text(
+                              projects[index],
+                              style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                              ),
                             ),
                           ),
 
-                          // Action Icons
-                          Row(
-                            children: [
-                              Image.asset(
+                          // Edit Icon
+                          GestureDetector(
+                              onTap: () {
+                                _openMemoBottomSheet(context);
+                              },
+                              child: Image.asset(
                                 "assets/editicon.png",
-                                height: 25,
-                              ),
-                              const SizedBox(width: 18),
-                              Image.asset(
-                                "assets/playicon.png",
                                 height: 28,
+                              )),
+
+                          // Timer
+                          if (isRunning)
+                            Text(
+                              _formatDuration(elapsed),
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.black87,
                               ),
-                            ],
-                          )
+                            ),
+                          const SizedBox(width: 12),
+
+                          // Play / Pause
+                          GestureDetector(
+                            onTap: () {
+                              if (isRunning) {
+                                _pauseTimer();
+                              } else {
+                                _startTimer(index);
+                              }
+                            },
+                            child: CircleAvatar(
+                              radius: 16,
+                              backgroundColor: AppColors.primaryColor,
+                              child: Icon(
+                                isRunning ? Icons.pause : Icons.play_arrow,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
                     );
@@ -150,4 +216,143 @@ class SelectProjectScreen extends StatelessWidget {
       ),
     );
   }
+// 📋 Bottom Sheet for Memo
+void _openMemoBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    backgroundColor: Colors.white,
+    context: context,
+    isScrollControlled: true,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+    ),
+    builder: (_) {
+      return Padding(
+        padding: EdgeInsets.only(
+          left: 16,
+          right: 16,
+          top: 16,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 16,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header
+            Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                const SizedBox(width: 8),
+                const Text(
+                  "Add Memo (Optional)",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+
+            // Select Date
+            TextField(
+              readOnly: true,
+              decoration: InputDecoration(
+                hintText: "Select Date",
+                 hintStyle: TextStyle(color: Colors.grey.shade400),
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+              
+                 border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: AppColors.primaryColor),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+              ),
+              onTap: () {
+                showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2100),
+                );
+              },
+            ),
+            const SizedBox(height: 12),
+
+            // Memo Field
+            TextField(
+              maxLines: 4,
+              decoration: InputDecoration(
+                hintText: "Write memo...",
+                hintStyle: TextStyle(color: Colors.grey.shade400),
+                alignLabelWithHint: true,
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+                border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: AppColors.primaryColor),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade400),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // Buttons Row
+            Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: AppColors.primaryColor),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 14),
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                    child: Text(
+                      "Projects",
+                      style: TextStyle(color: AppColors.primaryColor),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 14),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child:  Text("Manual Entry",style: TextStyle(color: Colors.white),),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
 }
